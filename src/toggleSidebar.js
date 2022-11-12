88d4a4aeab4bfd951d94e01dad20de94b4d7e91e
Brett Farmer <blfarmer89@yahoo.com>
2022-11-12 17:08:43 -0500

    Hero/Sidebar

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -1,1 +1,12 @@
 import { getElement } from './utils.js';
+
+const toggleNav = getElement('.toggle-nav');
+const sdieBarOverlay = getElement('.sidebar-overlay');
+const closeBtn = getElement('.sidebar-close');
+
+toggleNav.addEventListener('click', () => {
+  sdieBarOverlay.classList.add('show');
+});
+closeBtn.addEventListener('click', () => {
+  sdieBarOverlay.classList.remove('show');
+});
