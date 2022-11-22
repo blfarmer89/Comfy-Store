3c4c07f3ad6e0066e2f97db972caf0593282cfe8
Brett Farmer <blfarmer89@yahoo.com>
2022-11-21 21:07:46 -0500

    finished product

--- src/filters/search.js
+++ src/filters/search.js
@@ -13,7 +13,7 @@ const setupSearch = (store) => {
           return product;
         }
       });
-      display(newStore, getElement('.products-container'));
+      display(newStore, getElement('.products-container'), true);
       if (newStore.length < 1) {
         const products = getElement('.products-container');
         products.innerHTML = `
@@ -22,7 +22,7 @@ const setupSearch = (store) => {
         </h3>`;
       }
     } else {
-      display(store, getElement('.products-container'));
+      display(store, getElement('.products-container'), true);
     }
   });
 };
